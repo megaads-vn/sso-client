8a3dd56bb75319cb64b27f362addf085ab443c70
Bach Nguyen <bachnx2303@gmail.com>
2019-08-21 09:34:49 +0700

    feature: add service provider function

---
+++ src/SsoClientServiceProvider.php
@@ -0,0 +1,61 @@
+<?php 
+namespace Megaads\SsoClient;
+
+use Illuminate\Support\ServiceProvider;
+
+class SsoClientServiceProvider extends SsoClientServiceProvider 
+{
+
+    public function boot() 
+    {
+        $framework = $this->checkFrameWork();
+        if ($framework && $framework['key'] == 'laravel/framework' && $framework['version'] <= 52 ) {
+            include __DIR__ . '/routes.php';
+        } else {  
+            if ( method_exists($this, 'routesAreCached') ) {
+                if (!$this->app->routesAreCached()) {
+                    include __DIR__ . '/routes.php';
+                }
+            }
+        }
+        $this->publishConfig();
+    }
+
+    public function register() 
+    {
+
+    }
+
+    private function publishConfig()
+    {
+        if ( method_exists($this, 'config_path') ) {
+            $path = $this->getConfigPath();
+            $this->publishes([$path => config_path('sso-client.php')], 'config');
+        }
+    }
+
+    private function getConfigPath()
+    {
+        return __DIR__.'/../config/sso-client.php';
+    }
+
+    private function checkFrameWork() {
+        $findFrameWork = ['laravel/framework','laravel/lumen-framework'];
+        $frameworkDeclare = file_get_contents(__DIR__ . '../../../../../composer.json');
+        $frameworkDeclare = json_decode($frameworkDeclare, true);
+        $required =  array_key_exists('require', $frameworkDeclare) ? $frameworkDeclare['require'] : [];
+        $requiredKeys = [];
+        if ( !empty($required) ) {
+            $requiredKeys = array_keys($required);
+            foreach($requiredKeys as $key) {
+                if ( in_array($key, $findFrameWork) ) {
+                    $version = $required[$key];
+                    $version = str_replace('*', '',$version);
+                    $version = preg_replace('/\./', '', $version);
+                    return ['key' => $key, 'version' => (int) $version];
+                }
+            }
+        }
+        return NULL;
+    }
+}
