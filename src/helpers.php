7e011dd32df9d62639b10a4eb13797f80bd3f86f
Bach Nguyen <bachnx2303@gmail.com>
2022-03-07 12:13:03 +0700

    fix: Helper

--- src/helpers.php
+++ src/helpers.php
@@ -3,13 +3,16 @@
 if (!function_exists('ssoGetCache')) {
     function ssoGetCache($key, $default = '') {
         $retVal = $default;
-        $cacheType = \Config::get('sso.cache_type', 'cache');
+        $cacheType = \Config::get('sso.cache_type', 'session');
         switch ($cacheType) {
             case 'cache':
                 $retVal = \Cache::get($key);
                 break;
             case 'cookie':
                 $retVal = \Cookie::get($key);
+                break;
+            case 'session':
+                $retVal = \Session::get($key);
                 break;
         }
         return $retVal;
@@ -18,7 +21,7 @@ if (!function_exists('ssoGetCache')) {
 
 if (!function_exists('ssoSetCache')) {
     function ssoSetCache($key, $value, $time = 60) {
-        $cacheType = \Config::get('sso.cache_type', 'cache');
+        $cacheType = \Config::get('sso.cache_type', 'session');
         switch ($cacheType) {
             case 'cache':
                 \Cache::put($key, $value, $time);
@@ -26,19 +29,25 @@ if (!function_exists('ssoSetCache')) {
             case 'cookie':
                 \Cookie::queue($key, $value, $time);
                 break;
+            case 'session':
+                \Session::put($key, $value);
+                break;
         }
     }
 }
 
 if (!function_exists('ssoForgetCache')) {
     function ssoForgetCache($key) {
-        $cacheType = Config::get('sso.cache_type', 'cache');
+        $cacheType = Config::get('sso.cache_type', 'session');
         switch ($cacheType) {
             case 'cache':
                 \Cache::forget($key);
                 break;
             case 'cookie':
                 \Cookie::queue(\Cookie::forget($key));
+                break;
+            case 'session':
+                \Session::forget($key);
                 break;
         }
     }
